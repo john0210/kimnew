4048721ea9b61b3b9cb49f854190e4bfb570d278
Kim junghyun <john0210@hanmail.net>
2025-11-02 12:37:40 +0900

    251102 햄버거버튼 스크롤 이동시 움직이는 현상 제거, 이미지 교체

--- main.js
+++ main.js
@@ -3,20 +3,22 @@ document.addEventListener('DOMContentLoaded', () => {
     const navbarBurgerBack = document.querySelector('.navbar_burger_back');
     const navbarMenu = document.querySelector('.navbar_menu');
     const navbarOverlay = document.querySelector('.navbar_overlay');
-  
+
+    function openMenu() {
+        navbarBurger.style.display = 'none';       // 햄버거 버튼 숨김
+        navbarBurgerBack.style.display = 'block';  // X 버튼 보임
+        navbarMenu.classList.add('active');
+        navbarOverlay.classList.add('active');
+    }
 
     function closeMenu() {
-      navbarBurger?.classList.remove('active');
-      navbarMenu?.classList.remove('active');
-      navbarOverlay?.classList.remove('active');
+        navbarBurger.style.display = 'block';      // 햄버거 버튼 보임
+        navbarBurgerBack.style.display = 'none';   // X 버튼 숨김
+        navbarMenu.classList.remove('active');
+        navbarOverlay.classList.remove('active');
     }
-  
-    navbarBurger?.addEventListener('click', () => {
-      navbarBurger?.classList.toggle('active');
-      navbarMenu?.classList.toggle('active');
-      navbarOverlay?.classList.toggle('active');
-    });
-  
-    navbarBurgerBack?.addEventListener('click', closeMenu);
-    navbarOverlay?.addEventListener('click', closeMenu);
-  });
+
+    navbarBurger.addEventListener('click', openMenu);
+    navbarBurgerBack.addEventListener('click', closeMenu);
+    navbarOverlay.addEventListener('click', closeMenu);
+});
